6b06065963c7169eae889fd5626c6d41ae289dea
Ilya <runodj@gmail.com>
2019-03-03 19:14:12 +0300

    Fixed rotation

--- Core/Src/brushless_motor.c
+++ Core/Src/brushless_motor.c
@@ -16,14 +16,14 @@ void update_velocity(BrushlessMotor* BLDC, int8_t velocity)
 {
   if (velocity > 0) {
 		BLDC->rotation_dir = clockwise;
-  	BLDC->pwm_duty = velocity*PWM_RESIZE_COEF;
+  	BLDC->pwm_duty = SERVO_STOP_DUTY + velocity*PWM_RESIZE_COEF;
 	}
   else {
     BLDC->rotation_dir = counterclockwise;
-		BLDC->pwm_duty = (~velocity + 1)*PWM_RESIZE_COEF;
+		BLDC->pwm_duty = SERVO_STOP_DUTY - (~velocity + 1)*PWM_RESIZE_COEF;
   }
-	htim3.Instance->CCR1 = SERVO_STOP_DUTY + BLDC->pwm_duty;
-	htim3.Instance->CCR3 = SERVO_STOP_DUTY + BLDC->pwm_duty;
+	htim3.Instance->CCR1 = BLDC->pwm_duty;
+	htim3.Instance->CCR3 = BLDC->pwm_duty;
 }
 
 void motor_stop(BrushlessMotor *BLDC)
